e0e37b9e3ba30842f3cf4c5bab16f26663f54b33
Huy Phan Anh Ha <phananhalbert@gmail.com>
2025-08-05 16:31:23 -0400

    middleware 2 test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,32 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export default function middleware(req) {
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/(.*)",
+  "/",
+  "/casino",
+  "/casino/poker",
+  "/casino/blackjack",
+  "/casino/roulette",
+  "/casino/uno",
+  "/access-denied",
+  "/complete-profile",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isPublicRoute(req)) {
+    return NextResponse.next();
+  }
+
+  auth.protect(); // Only protect private routes
   return NextResponse.next();
-}
+});
 
 export const config = {
-  matcher: ['/((?!_next|.*\\..*).*)', '/(api|trpc)(.*)'],
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
