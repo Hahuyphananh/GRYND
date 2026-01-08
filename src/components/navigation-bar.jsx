eaeacc7e8a1bffd9b367c07346865f13bfe280ff
Huy Phan Anh Ha <phananhalbert@gmail.com>
2026-01-07 21:59:22 -0500

    Changed logo design

--- src/components/navigation-bar.jsx
+++ src/components/navigation-bar.jsx
@@ -83,7 +83,7 @@ function NavigationBar({ currentPath }) {
           <Link href="/" className="flex items-center space-x-2">
  <Image
   src={LogoSmiley}
-  alt="GoonBet Logo"
+  alt="MoonBet Logo"
   width={150}
   height={60}
   className="drop-shadow-[0_0_8px_rgba(255,215,0,0.3)] rounded-lg object-contain"
