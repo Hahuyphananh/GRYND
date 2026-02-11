f7814a78a6c0cb6572fd1ad829d087ac97d20e11
Huy Phan Anh Ha <phananhalbert@gmail.com>
2026-02-10 22:16:50 -0500

    coin-flip design fix and routes added

---
+++ src/app/api/get-user/route.js
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+export async function GET() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return NextResponse.json({ success:false });
+  }
+
+  return NextResponse.json({
+    success:true,
+    data:{ userId }
+  });
+}
